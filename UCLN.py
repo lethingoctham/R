efad0fead66c4009b33a9700cb4231202cb51a52
lethingoctham <72124921+lethingoctham@users.noreply.github.com>
2021-05-07 10:42:18 +0700

    Delete UCLN.py

--- UCLN.py
+++
@@ -1,8 +0,0 @@
-a = int(input('nhap a = '))
-b = int(input('nhap b = '))
-
-def uscln(a, b):
-    if (b == 0):
-        return a;
-    return uscln(b, a % b);
-print('UCLN cua',a,'va ',b,'la :',uscln(a,b))
